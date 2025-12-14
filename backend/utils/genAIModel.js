b29dedbe0a03956e6ddb0adba15af5682c3f4a3e
ami1manna <amitmannasm@gmail.com>
2025-12-14 14:34:34 +0530

    changed model

--- backend/utils/genAIModel.js
+++ backend/utils/genAIModel.js
@@ -2,6 +2,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const geminiApiKey = process.env.GEMINI_API;
 const genAI = new GoogleGenerativeAI(geminiApiKey);
-const genAIModel = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+const genAIModel = genAI.getGenerativeModel({ model: "gemini-flash-latest" });
 
 export default genAIModel;
